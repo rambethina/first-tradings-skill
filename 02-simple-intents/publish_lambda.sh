d4017c1fe42a400b7d3093d6265f2e233e041cdb
vijayaram bethina <vijayarambethina@vijayarams-Mac-mini.local>
2018-10-10 07:56:39 -0500

    first commit

--- 02-simple-intents/publish_lambda.sh
+++
@@ -1,6 +0,0 @@
-rm lambda.zip
-cd lambda
-npm install
-zip -r ../lambda.zip *
-cd ..
-aws lambda update-function-code --function-name voice-devs --zip-file fileb://lambda.zip --profile oscar-eu-west-1
--- 09-using-the-meetup-api-2/lambda/helpers/getLondonAudio.js
+++
@@ -1,9 +0,0 @@
-module.exports = function getLondonAudio(city) {
-
-  var londonAudio = ``;
-  if (city.toLowerCase() === 'london') {
-    londonAudio = `<audio src="https://s3-eu-west-1.amazonaws.com/voice-devs/london-baby.mp3"/>`;
-  }
-
-  return londonAudio;
-};
